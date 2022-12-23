8f7be7f6809120bd7b904044e17d3048fbe7d62a
Yasmeenelsayd <121091590+Yasmeenelsayd@users.noreply.github.com>
2022-12-23 14:15:12 +0200

    Add files via upload

---
+++ perfect numbers project.cs
@@ -0,0 +1,34 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+
+class program
+{
+    
+    static void Main(string [] args)
+    {
+        Console.WriteLine("Enter n1");
+        int n1 = int.Parse(Console.ReadLine());
+        Console.WriteLine("Enter n2");
+        int n2 = int.Parse(Console.ReadLine());
+
+        Console.WriteLine("The perfect numbers from" + n1 + "to" + n2 + "are :");
+
+        for (int i=n1; i <= n2; i++)
+        {
+            int sum = 0;
+
+            for (int j=1 ; j <i ; j++)
+            {
+                if (i % j == 0)
+                {
+                    sum += j;
+                }
+            }
+            if (sum == i)
+            {
+                Console.WriteLine(i);
+            }
+        }
+        Console.ReadKey();
+    }
+}
---
+++ prime numer project.cs
@@ -0,0 +1,34 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Enter n1");
+        int n1= int.Parse(Console.ReadLine());
+        Console.WriteLine("Enter n2");
+        int n2 = int.Parse(Console.ReadLine());
+
+        Console.WriteLine("The prime numbers between " + n1 + "and" + n2 + "are :");
+
+        for (int i=n1; i <= n2 ; i++)
+        {
+            int counter = 0;
+
+            for (int j=2; j<= i/2 ; j++)
+            {
+                if (i%j==0)
+                {
+                    counter++;
+                    break;
+                }
+            }
+            if (counter== 0 && i != 1)
+            {
+                Console.WriteLine(i);
+            }
+        }
+        Console.ReadKey();
+
+    }
+}
